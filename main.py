186c154190ca958d496e7daa521b61aa5bf90570
Soufiane MELLOUKI <soufiane.mellouki@epsi.fr>
2022-10-25 10:47:54 +0200

    Changement du nom du script pour être conforme

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import csv
+
+with open('conso-annuelles_v1.csv',newline='', encoding='latin1') as f:
+    tableau=[]
+    lire=csv.reader(f, delimiter=";")
+    print('',end='\n')
+    print('Affichage des lignes du tableau',end='\n')
+    for ligne in lire:
+        print(ligne, end='\n')
+        tableau.append(ligne)
