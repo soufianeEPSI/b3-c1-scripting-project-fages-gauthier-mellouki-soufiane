f62d8985da6ac62559c49a6f977d324b944b275e
Soufiane MELLOUKI <soufiane.mellouki@epsi.fr>
2022-10-25 10:44:26 +0200

    Changement de delimiter pour avoir les bon champs dans le tableau

--- csv_extraction.py
+++ csv_extraction.py
@@ -3,7 +3,7 @@ import csv
 
 with open('conso-annuelles_v1.csv',newline='', encoding='latin1') as f:
     tableau=[]
-    lire=csv.reader(f)
+    lire=csv.reader(f, delimiter=";")
     print('',end='\n')
     print('Affichage des lignes du tableau',end='\n')
     for ligne in lire:
