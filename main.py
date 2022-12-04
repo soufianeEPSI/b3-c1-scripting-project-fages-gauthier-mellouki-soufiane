0ab3b0c6917362b4e8b63df10fbfc631ea5a38f5
Soufiane MELLOUKI <soufiane.mellouki@epsi.fr>
2022-12-04 22:19:18 +0100

    Fonction somme des consommations des deux années dans une nouvelle colonne

--- main.py
+++ main.py
@@ -28,7 +28,23 @@ def suppr_ligne_champvide(tableau):
             j-=1
         i-=1
 
+#Cette fonction additionne les consommations des deux années et ajoute le résultat dans une colonne supplémentaire
+def add_conso(tableau):
+    i=len(tableau)-1
+    while i > 0:
+        # J'utilise un bloc try except pour repérer les erreur et les traiter.
+        # Dans les deux colonnes, les valeurs numériques sont écrites sous la forme européennes, avec une virgule, et pas un point.
+        try:
+            a = tableau[i][1].replace(',','.') # Je remplace dans la chaine de caractères la virgule par un point
+            b = tableau[i][2].replace(',','.')
+            total=float(a)+float(b) # Je convertis les chaines de caractères en nombres réels à virgule flottante et les additionne
+            tableau[i].append(total) # J'ajoute une nouvelle colonne au tableau où je stocke la somme obtenue
+        except:
+            tableau.pop(i) # Le bloc except m´a indiqué la ligne non conforme: ['Piscines', '5033,1', '-', 'SPA'], je supprime la ligne du tableau
+        i-=1
+
 tableau=csv_to_array('conso-annuelles_v1.csv')
 suppr_col_IDlogement(tableau)
 suppr_ligne_champvide(tableau)
+add_conso(tableau)
 print(tableau)
