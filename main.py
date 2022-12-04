bf6ba8cb1ae33bbfe300f7dfd8731dcd7d07cc51
Soufiane MELLOUKI <soufiane.mellouki@epsi.fr>
2022-12-04 19:21:55 +0100

    Fonction qui prend en entrée le CSV et retourne ses données dans un tableau

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import csv
 
-with open('conso-annuelles_v1.csv',newline='', encoding='latin-1') as f:
-    tableau=[]
-    lire=csv.reader(f, delimiter=";")
-    print('',end='\n')
-    print('Affichage des lignes du tableau',end='\n')
-    for ligne in lire:
-        print(ligne, end='\n')
-        tableau.append(ligne)
+#Cette fonction va ouvrir le fichier CSV
+def csv_to_array(fichier_csv):
+    with open('conso-annuelles_v1.csv',newline='', encoding='latin-1') as f:
+        tableau=[]
+        lire=csv.reader(f, delimiter=";")
+        for ligne in lire:
+            tableau.append(ligne)
+    return tableau
+
+print(csv_to_array('conso-annuelles_v1.csv'))
