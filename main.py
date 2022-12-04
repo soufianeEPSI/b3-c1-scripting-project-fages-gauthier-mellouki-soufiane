6864c6988c703399f5a5c9be69dc363475abd05b
Soufiane MELLOUKI <soufiane.mellouki@epsi.fr>
2022-12-04 19:33:26 +0100

    Fonction qui supprime la colonne ID Logement

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 import csv
 
-#Cette fonction va ouvrir le fichier CSV
+#Cette fonction va ouvrir le fichier CSV et le stocker dans un tableau et le retourner en résultat
 def csv_to_array(fichier_csv):
     with open('conso-annuelles_v1.csv',newline='', encoding='latin-1') as f:
         tableau=[]
-        lire=csv.reader(f, delimiter=";")
+        lire=csv.reader(f, delimiter=";") #On crée un tableau unidimensionnel avec chaque champ de la ligne du CSV délimitée par ";"
         for ligne in lire:
-            tableau.append(ligne)
+            tableau.append(ligne) #On ajoute chaque tableau unidimensionnel (donc chaque ligne du CSV) dans un tableau global avec tout le contenu du fichier CSV
     return tableau
 
-print(csv_to_array('conso-annuelles_v1.csv'))
+def suppr_col_IDlogement(tableau):
+    for ligne in tableau:
+        ligne.pop(1)
+        print(ligne)
+
+tableau=csv_to_array('conso-annuelles_v1.csv')
+suppr_col_IDlogement(tableau)
